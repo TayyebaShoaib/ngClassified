e4af453b88f97c20b29c28b6bf9b823e64171135
TayyebaShoaib <Tayyeba.s@pearl-dme.com>
2018-04-19 14:22:54 +0100

    The Toolbar and Classified Card

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -5,6 +5,11 @@
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope)
         {
-            $scope.message = "Hello, world!!";
+            $scope.classified =
+            {
+                title: 'First Item',
+                price: '$1,000,000',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            };
         });
 })();
